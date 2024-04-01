984bb53291a8c076d91298026ac6ec2736c17414
Aldo Hartanto <aldohartanto06@gmail.com>
2024-04-01 19:43:40 +0700

    progress signin

--- client/src/components/common/SigninForm.jsx
+++ client/src/components/common/SigninForm.jsx
@@ -1,8 +1,13 @@
-import { LoadingButton } from "@mui/lab"
-import { Alert, Box, Button, Stack, TextField} from "@mui/material"
-import { useState } from "react"
-import { useFormik } from "formik"
-
+import { LoadingButton } from "@mui/lab";
+import { Alert, Box, Button, Stack, TextField } from "@mui/material";
+import { useFormik } from "formik";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import * as Yup from "yup";
+import userApi from "../../api/modules/user.api";
+import { setAuthModalOpen } from "../../redux/features/authModalSlice";
+import { setUser } from "../../redux/features/userSlice";
 
 const SigninForm = ({switchAuthstate}) => {
   return (
