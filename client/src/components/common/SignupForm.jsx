b9f95e8d30ea378e4f0363b0b746f3495ccffaac
Aldo Hartanto <aldohartanto06@gmail.com>
2024-04-01 21:11:38 +0700

    add sign up

--- client/src/components/common/SignupForm.jsx
+++ client/src/components/common/SignupForm.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
-
+import { LoadingButton } from "@mui/lab";
+import { Alert, Box, Button, Stack, TextField } from "@mui/material";
+import { useFormik } from "formik";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import * as Yup from "yup";
+import userApi from "../../api/modules/user.api";
+import { setAuthModalOpen } from "../../redux/features/authModalSlice";
+import { setUser } from "../../redux/features/userSlice";
 const SignupForm = ({switchAuthstate}) => {
   return (
     <div>
