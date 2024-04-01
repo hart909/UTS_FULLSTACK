76175f710a6fe762cda7d34fb63b4889b8c6abaf
Aldo Hartanto <aldohartanto06@gmail.com>
2024-04-01 19:42:12 +0700

    progress sign in

---
+++ client/src/components/common/SigninForm.jsx
@@ -0,0 +1,15 @@
+import { LoadingButton } from "@mui/lab"
+import { Alert, Box, Button, Stack, TextField} from "@mui/material"
+import { useState } from "react"
+import { useFormik } from "formik"
+
+
+const SigninForm = ({switchAuthstate}) => {
+  return (
+    <div>
+      Sign In
+    </div>
+  )
+}
+
+export default SigninForm
