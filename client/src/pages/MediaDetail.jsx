48e0b99b3c960670a8a760f81a1a0b347820da32
Aldo Hartanto <aldohartanto06@gmail.com>
2024-04-09 22:42:27 +0700

    new

--- client/src/pages/MediaDetail.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MediaDetail = () => {
-  return (
-    <div>MediaDetail</div>
-  )
-}
-
-export default MediaDetail
--- client/src/pages/PersonDetail.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PersonDetail = () => {
-  return (
-    <div>PersonDetail</div>
-  )
-}
-
-export default PersonDetail
