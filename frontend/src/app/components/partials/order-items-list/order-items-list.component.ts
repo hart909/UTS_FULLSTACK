0e5743d5adf99b03b7b5ff2196478aabb8b1d2fb
Givvven <given.535220143@stu.untar.ac.id>
2024-05-04 18:23:22 +0700

    given

--- frontend/src/app/components/partials/order-items-list/order-items-list.component.ts
+++ frontend/src/app/components/partials/order-items-list/order-items-list.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Order } from '../../../shared/models/Order';
 
 @Component({
-  selector: 'app-order-items-list',
+  selector: 'order-items-list',
   templateUrl: './order-items-list.component.html',
   styleUrl: './order-items-list.component.css'
 })
 export class OrderItemsListComponent {
+  
+  @Input()
+  order!:Order;
 
 }
