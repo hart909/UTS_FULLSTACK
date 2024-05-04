5c51fe7ce729d541632828d873e958752fa411ee
Givvven <given.535220143@stu.untar.ac.id>
2024-05-04 19:54:06 +0700

    plss

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { FilmPageComponent } from './components/pages/film-page/film-page.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
+import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'search/:searchTerm', component:HomeComponent},
+  {path:'tag/:tag', component:HomeComponent},
+  {path:'film/:id', component:FilmPageComponent},
+  {path: 'cart-page', component: CartPageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'checkout', component: CheckoutPageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
