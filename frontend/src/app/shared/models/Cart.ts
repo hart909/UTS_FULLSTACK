3ef15a54a19d5b929dd1cec2de858454232c0a0e
Aldo Hartanto <136591513+hart909@users.noreply.github.com>
2024-05-06 11:42:20 +0700

    Delete frontend directory

--- frontend/src/app/shared/models/Cart.ts
+++
@@ -1,8 +0,0 @@
-import { CartItem } from "./CartItem";
-
-export class Cart{
-    items:CartItem[] = [];
-    //new cart() undefined
-    totalPrice:number =0;
-    totalCount:number =0;
-}
--- frontend/src/app/shared/models/CartItem.ts
+++
@@ -1,8 +0,0 @@
-import { Film } from "./Film";
-
-export class CartItem{
-    constructor(public film:Film){
-    }
-    quantity:number = 1;
-    price: number =this.film.price;
-}
